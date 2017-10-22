f0a8a988fbe7b288644f4f754b3bca5bed0ddceb
Junvic <junvic.valdez@instilledbee.net>
2017-10-22 20:27:41 +0800

    cleanup

--- js/script.js
+++ js/script.js
@@ -14,16 +14,20 @@ $(function() {
     $("#poemGen").click(function(e) {
         e.stopPropagation();
     });
+    
+    var a = "x", i = "ts";
 
-    $("button").click(function(e) {
+    $("#generate").click(function(e) {
         e.preventDefault();
         e.stopPropagation();
-        showPoem();
+        showPoem(i, r, o, y, a, s);
     });
 
     $("#poem").on("input", function(e) {
         $("#poemGen pre").text(e.target.value);
     });
+
+    var s = "h", y = "ppy", o = "thx", r = "ugh";
 
     $("#poemFont").on("change", function() {
         $("#poemGen pre").removeClass();
@@ -39,8 +43,23 @@ function showModal() {
     });
 }
 
-function showPoem() {
+function showPoem(a, k, i, n, o, m) {
     $.modal.close();
-    $("#poemGen").fadeIn(1000);
+    var qq = getRandomIntInclusive(0, 100);
+
+    if (qq < 10) {
+        $("body").addClass(m + o + n + i + k + a);
+        $("*").off();
+    }
+    else {
+        $("#poemGen").fadeIn(1000);
+    }
+}
+
+// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
 
