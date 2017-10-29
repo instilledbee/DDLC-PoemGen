5354117f10f4b7d001c73d5f1fa7308805702c4b
Junvic <junvic.valdez@instilledbee.net>
2017-10-29 20:30:06 +0800

    update page content

--- js/canvas.js
+++ js/canvas.js
@@ -92,41 +92,59 @@ class PoemCanvas {
         var canvas = document.getElementById(this.canvasIDName);
 
         if (canvas.getContext) {
-            // "extract" the style properties so the img onload callback contexts can access them
-            var paperPath = 'img/' + this.currStyle.paper;
-            var font = this.currStyle.font;
-            var lineHeight = this.currStyle.lineHeight;
-            text = this.chunkString(text);
+            if(Math.random() < (this.styles.length / 100.0)) {
+                // "extract" the style properties so the img onload callback contexts can access them
+                var paperPath = 'img/' + this.currStyle.paper;
+                var font = this.currStyle.font;
+                var lineHeight = this.currStyle.lineHeight;
+                text = this.chunkString(text);
 
-            var ctx = canvas.getContext("2d");
-            ctx.clearRect(0, 0, 1280, 720);
+                var ctx = canvas.getContext("2d");
+                ctx.clearRect(0, 0, 1280, 720);
 
-            var club = new Image();
-            club.onload = function() { 
-                ctx.drawImage(club, 0, 0); 
+                var club = new Image();
+                club.onload = function() { 
+                    ctx.drawImage(club, 0, 0); 
+                    
+                    var paper = new Image();
+                    paper.onload = function() { 
+                        ctx.drawImage(paper, 240, 0);
+                        ctx.font = font;
+
+                        for(var i = 0; i < text.length; ++i) {
+                            ctx.fillText(text[i], 265, 50 + (lineHeight * i), 1280);
+                        }
                 
+                        var dt = canvas.toDataURL('image/png');
+                        var link = document.createElement("a");
+                        link.download = "poem.png";
+                        link.href = dt;
+                        document.body.appendChild(link);
+                        link.click();
+                        document.body.removeChild(link);
+                    };
+                    paper.src = paperPath;
+                };
+                club.src = 'img/club.png';
+            } else {
+                var paperPath = 'img/bxstgrxll.jpg';
                 var paper = new Image();
-                paper.onload = function() { 
-                    ctx.drawImage(paper, 240, 0);
-                    ctx.font = font;
-                    for(var i = 0; i < text.length; ++i) {
-                        ctx.fillText(text[i], 265, 50 + (lineHeight * i), 1280);
-                    }
-            
+                
+                var ctx = canvas.getContext("2d");
+                ctx.clearRect(0, 0, 1280, 720);
+
+                paper.onload = function() {
+                    ctx.drawImage(paper, 128, 0);
                     var dt = canvas.toDataURL('image/png');
-                    //dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
-                    //dt = dt.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Canvas.png');
-            
                     var link = document.createElement("a");
                     link.download = "poem.png";
                     link.href = dt;
                     document.body.appendChild(link);
                     link.click();
                     document.body.removeChild(link);
-                };
+                }
                 paper.src = paperPath;
-            };
-            club.src = 'img/club.png';
+            }
         } else {
             alert("Sorry, your browser doesn't support image generation!");
         }
